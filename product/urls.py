e171171e5d49075697bb7afad4fdba6f4afcacb2
Ilgiz <ilgizdj99@gmail.com>
2021-11-03 16:15:59 +0600

    CRUD', CBV

---
+++ product/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import *
+from .class_views import *
+
+urlpatterns = [
+    path('', CategoryListView.as_view(), name='home'),
+    path('<str:slug>/', ProductListView.as_view(), name='list'),
+    path('product/<int:product_id>/', ProductDetailView.as_view(), name='detail'),
+    path('product/create/', product_create, name='product-create'),
+    path('product/update/<int:product_id>', product_update, name='product_update'),
+    path('product/delete/<int:product_id>', product_delete, name='product_delete'),
+]
--- shop/settings.py
+++ shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -57,7 +58,9 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
